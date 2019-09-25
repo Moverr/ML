3251e654b074f3c3258bdb69a970ef0cb936f8fc
Muyinda Rogers <moverr@gmail.com>
2019-09-25 20:15:24 +0300

    :Debugging

--- firstmlproject.py
+++ firstmlproject.py
@@ -9,4 +9,17 @@ features = [
 ]
 labels = [0,0,1,1]
 
+# Decision Tree Classifier 
 clf = tree.DecisionTreeClassifier()
+# Find Pattens in Data FIT 
+clf.fit(features,labels)
+
+result = (clf.predict([[110,1]]))
+
+result_extp = { }
+result_extp[0] = "Orange"
+result_extp[1] = "Apple"
+ 
+# print (result)
+print(result_extp[result[0]])
+
