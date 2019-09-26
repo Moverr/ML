c2de3de8b0b507d76662214d03aba54eb3722d28
Muyinda Rogers <moverr@gmail.com>
2019-09-27 01:56:37 +0300

    :Debugging

--- pipeline.py
+++ pipeline.py
@@ -9,3 +9,8 @@ x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = .5)
 from sklean import tree
 my_classifier = tree.DecissionTreeClassifier()
 my_classifier.fit(x_train,y_train)
+
+predictions = my_classifier.predict(x_test)
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(y_test,predictions)
