2d21a7f8dd57b042ece59c5cb7eea58a5b8a25e5
Muyinda Rogers <moverr@gmail.com>
2019-09-27 01:57:27 +0300

    pipelines

--- pipeline.py
+++ pipeline.py
@@ -6,8 +6,8 @@ y = iris.target
 from sklearn.model_selection import  train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = .5)
 
-from sklean import tree
-my_classifier = tree.DecissionTreeClassifier()
+from sklearn import tree
+my_classifier = tree.DecisionTreeClassifier()
 my_classifier.fit(x_train,y_train)
 
 predictions = my_classifier.predict(x_test)
