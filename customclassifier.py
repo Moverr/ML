d02c833edec28c076519fec461bedfc427e8775f
Muyinda Rogers <moverr@gmail.com>
2019-09-27 13:42:24 +0300

    Custom Classifier

--- customclassifier.py
+++ customclassifier.py
@@ -1,3 +1,6 @@
+class ScarppyKNN:
+
+
 from sklearn import datasets
 iris = datasets.load_iris()
 x = iris.data
@@ -10,8 +13,13 @@ x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = .5)
 # from sklearn import tree
 # my_classifier = tree.DecisionTreeClassifier()
 
-from sklearn.neighbors import  KNeighborsClassifier
-my_classifier = KNeighborsClassifier()
+# from sklearn.neighbors import  KNeighborsClassifier
+# my_classifier = KNeighborsClassifier()
+
+
+# Customer Classifier 
+my_classifier = ScarppyKNN()
+
 
 my_classifier.fit(x_train,y_train)
 
