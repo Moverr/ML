d6622c44c406a1b6c6156c80f3a7960145842a5c
Muyinda Rogers <moverr@gmail.com>
2019-10-01 18:36:22 +0300

    :Debugging

--- categoryspeciealizer.py
+++ categoryspeciealizer.py
@@ -10,8 +10,21 @@ olddata= [
     
     ]
 
-for i in olddata:
-    print(i)
+d = {}
+for x,y in olddata:
+    count = 0
+    if(len(d) > 0 ):
+        for z in d.keys():
+            if(z == x):
+                count = d[z] + 1
+    else:
+        count = 1
+                
+    print("COUNT : ")
+    print(count)
+    d[x] = y
+
+print(d)
 print(olddata)
 def bayern_probabilyt():
     pass
