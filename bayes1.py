ce21be0328bbea02db62358302b7dcc24ff1c66c
Muyinda Rogers <moverr@gmail.com>
2019-10-01 16:01:11 +0300

    Loan Defaulters

---
+++ bayes1.py
@@ -0,0 +1,7 @@
+from datasets import load_loan_defaulters
+from feature import ContinuousFeature
+from feature import DiscreteFeature
+from naive_bayes import NaiveBayes
+
+
+# todo import data from samples 
