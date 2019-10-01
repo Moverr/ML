9e7921481671f3eeca347f9151e0a81716b22e85
Muyinda Rogers <moverr@gmail.com>
2019-10-01 12:47:56 +0300

    :Debugging

--- customclassifier.py
+++ customclassifier.py
@@ -17,7 +17,15 @@ class ScarppyKNN():
             predictions.append(label)
         return predictions
 def closest(self,row):
-    pass
+    best_dist = euc(row,self.x_train[0])
+    best_index = 0
+    for i in range(1,len(self.x_train)):
+        dist = euc(row,self.x_train[i])
+        if dist < best_dist:
+            best_dist = dist
+            best_index = i
+
+    return self.y_train[best_index]
 
 
 from sklearn import datasets
