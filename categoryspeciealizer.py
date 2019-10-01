3a9cf2c88f6fb1b119bf1a39e2d30a5ddd4744b6
Muyinda Rogers <moverr@gmail.com>
2019-10-01 20:11:42 +0300

    :Debugging

--- categoryspeciealizer.py
+++ categoryspeciealizer.py
@@ -8,30 +8,7 @@ olddata= [
     [1,1]
     ]
 
-d = {}
-first_record = olddata[0]
-
-# get the view count 
-for x,y in olddata:
-    count = 1
-    for keys in d.keys():
-        if(keys == x):            
-            count = d[keys] + 1
-    d[x] = count
-
-# todo: loop through and get the probability list of viewership vs category 
-x = {}
  
-total_sum = sum(d.values())
- 
-for keys in d.keys():
-    percentile = float(d[keys]) /float(total_sum) * 100 
-    x[keys] = percentile
-
-
-print(x)
-print(olddata)
-
 def clean_data(sample_data):
     records = {}
     first_record = sample_data[0] 
