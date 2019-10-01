6f17f26083da0210d86e0a0f9166b6c487059dc6
Muyinda Rogers <moverr@gmail.com>
2019-10-01 16:43:33 +0300

    :Debuging

--- bayes1.py
+++ bayes1.py
@@ -16,9 +16,24 @@ print(dataset)
 # todo import data from samples 
 
 import numpy as np
-data_coin_flips = np.random.randint(2, size=1000)
+data_coin_flips = np.random.randint(2, size=5)
 
+print(":::::::::::::::::::::::::::::::")
+print(data_coin_flips)
 result = np.mean(data_coin_flips)
 
 print("-------------------------------------")
 print(result)
+
+
+import scipy.stats as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+# %matplotlib inline
+sns.set(style='ticks', palette='Set2')
+
+params = np.linspace(0, 1, 100)
+p_x = [np.product(st.bernoulli.pmf(data_coin_flips, p)) for p in params]
+plt.plot(params, p_x)
+sns.despine()
+
