e6b8137601463160fa2790ef1825a57d09c544f0
Muyinda Rogers <moverr@gmail.com>
2019-10-01 19:48:49 +0300

    :Debugging

--- categoryspeciealizer.py
+++ categoryspeciealizer.py
@@ -25,12 +25,24 @@ x = {}
 total_sum = sum(d.values())
  
 for keys in d.keys():
-    
     percentile = float(d[keys]) /float(total_sum) * 100 
     x[keys] = percentile
 
+
 print(x)
 print(olddata)
+def clean_data(sample_data):
+    record = {}
+    first_record = sample_data[0]
+    # get the view count 
+    for x,y in sample_data:
+        count = 1
+        for keys in record.keys():
+            if(keys == x):            
+                count = record[keys] + 1
+        record[x] = count
+
+ 
 def bayern_probabilyt():
     pass
 
