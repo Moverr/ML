48a4c1180b7b4ff6ecee794f52b7b783d113685f
Muyinda Rogers <moverr@gmail.com>
2019-10-01 16:14:14 +0300

    :Debugging

--- bayes1.py
+++ bayes1.py
@@ -1,7 +1,15 @@
-from datasets import load_loan_defaulters
+
 from feature import ContinuousFeature
 from feature import DiscreteFeature
 from naive_bayes import NaiveBayes
+
+
+import pandas  
+url =  "iris2.csv"
+names =  ["D1","D2","D3","D4","D5","D6","D7","D8","D9","d10","d11"]
+# always describe names against the column dataset 
+dataset = pandas.read_csv(url, names=names)
+
 
 
 # todo import data from samples 
