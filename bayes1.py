c05e747788b65c011047f87b19ce6facfd0c966e
Muyinda Rogers <moverr@gmail.com>
2019-10-01 16:24:23 +0300

    :Debugging

--- bayes1.py
+++ bayes1.py
@@ -5,7 +5,7 @@
 
 
 import pandas  
-url =  "iris2.csv"
+url =  "loan-defaulters.csv"
 names =  ["D1","D2","D3","D4","D5","D6","D7","D8","D9","d10","d11"]
 # always describe names against the column dataset 
 dataset = pandas.read_csv(url, names=names)
