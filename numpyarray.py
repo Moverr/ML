d1fba2eaf6c9adf7ae58a8beea905550ba74bb8b
Muyinda Rogers <moverr@gmail.com>
2019-10-23 17:16:36 +0300

    Intersting findings

---
+++ numpyarray.py
@@ -0,0 +1,6 @@
+import numpy as np 
+
+dataset = [[1,2,3],[3,4,5],[6,7,8],[11,12,34]]
+#convert dataset to numpy aarrya ndArray 
+ndArray = np.array(dataset)
+print("Numpy Array X:\n{}".format(ndArray))
