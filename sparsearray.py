8a1ff40ae135cf2405ff702c0d414037a926f89a
Muyinda Rogers <moverr@gmail.com>
2019-10-23 17:32:15 +0300

    :Debugging

--- sparsearray.py
+++ sparsearray.py
@@ -1,9 +1,14 @@
 import numpy as np 
 from  scipy import sparse 
 
-
+ 
 dataset = [[1,2,3],[3,4,5],[6,7,8],[11,12,34]]
 #convert dataset to numpy aarrya ndArray 
 ndArray = np.array(dataset)
-eye = np.eye(4)
-print("Numpy Array X:\n{}".format(ndArray))
+
+
+eye = np.eye(23)
+sparse = sparse.csr_matrix(dataset)
+
+print("Sci Array X:\n{}".format(dataset))
+print("Sci Array X:\n{}".format(sparse))
