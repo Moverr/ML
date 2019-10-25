a7483838386484cefca408cedf443cff02f65111
Muyinda Rogers <moverr@gmail.com>
2019-10-25 18:52:27 +0300

    interesting, Shape of Data in Pandas

--- IrisdatasetML.py
+++ IrisdatasetML.py
@@ -3,5 +3,9 @@ from sklearn.datasets import load_iris
 irisDataset = load_iris()
 
 print("The Iris Dataset {}".format(irisDataset.keys()))
-print("---------------------------------------------------")
-print("  XXX {} \n ".format(type(irisDataset['data'])))
+print("----------------------Type of Data-----------------------------")
+print("  Type of Data {} \n ".format(type(irisDataset['data'])))
+
+print("----------------------Shapee of Data-----------------------------")
+print("  Shape of Data {} \n ".format(irisDataset['data'].Shape))
+
