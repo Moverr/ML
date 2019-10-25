13dd9f02b36a355de754331264604d7394c48b98
Muyinda Rogers <moverr@gmail.com>
2019-10-25 12:23:52 +0300

    Plotting, quite interesting

--- matplotlibexample.py
+++ matplotlibexample.py
@@ -1,2 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np 
+
+x= np.linspace(-10,10,100)
+y = np.sin(x)
+
+plt.plot(x,y,marker="x")
+plt.show()
--- numpyarray.py
+++ numpyarray.py
@@ -1,5 +1,6 @@
 import numpy as np 
 
+
 # convert list into array 
 dataset = [[1,2,3],[3,4,5],[6,7,8],[11,12,34]]
 #convert dataset to numpy aarrya ndArray 
