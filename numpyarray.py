60d60ca99a09def8b18002092abf4c302120c820
Muyinda Rogers <moverr@gmail.com>
2019-10-25 11:53:37 +0300

    debuging

--- numpyarray.py
+++ numpyarray.py
@@ -1,6 +1,8 @@
 import numpy as np 
 
+# convert list into array 
 dataset = [[1,2,3],[3,4,5],[6,7,8],[11,12,34]]
 #convert dataset to numpy aarrya ndArray 
 ndArray = np.array(dataset)
+
 print("Numpy Array X:\n{}".format(ndArray))
