76219dde24bdf5c17a3a66d7e3b690ac1dc8732c
Muyinda Rogers <moverr@gmail.com>
2019-10-25 16:25:18 +0300

    Creating a dataframe from an object json, using python pandas

--- createdataframe.py
+++ createdataframe.py
@@ -3,6 +3,8 @@ import pandas as pd
 data = {
     "Name":["John","Anna","Peter","Linda"],
     "Location":["New York","Paris","Berlin","London"],
-    "Age":[24,13,53,33],
-    
+    "Age":[24,13,53,33]   
 }
+
+dataFrame = pd.DataFrame(data)
+print(dataFrame)
