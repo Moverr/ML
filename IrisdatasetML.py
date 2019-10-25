beadc46db9646c992630a3b9e50defff3157f2b9
Muyinda Rogers <moverr@gmail.com>
2019-10-25 18:57:02 +0300

    :Debuging

--- IrisdatasetML.py
+++ IrisdatasetML.py
@@ -7,5 +7,9 @@ print("----------------------Type of Data-----------------------------")
 print("  Type of Data {} \n ".format(type(irisDataset['data'])))
 
 print("----------------------Shapee of Data-----------------------------")
-print("  Shape of Data {} \n ".format(irisDataset['data'].Shape))
+print("  Shape of Data {} \n ".format(irisDataset['data'].shape))
+
+
+print("----------------------First FIve Columns of Data -----------------------------")
+print("  First five columns  of Data {} \n ".format(irisDataset['data'][:5]))
 
