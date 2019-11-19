0bc099f373ae94e2a3ad4d9c8aec7afb67255099
Muyinda Rogers <moverr@gmail.com>
2019-11-19 15:12:38 +0300

    :Debugging :

--- knn.py
+++ knn.py
@@ -1,15 +1,10 @@
 # upgrading a package 
 # pip3 install --upgrade pandas --user
 import numpy as np
-
 print("NUMPY {}".format(np.__version__))
 import scipy as sc
-
 print("SCIPY {}".format(sc.__version__))
-
 import pandas as pd 
-
-
 print("PANDA {}".format(pd.__version__))
 
 
