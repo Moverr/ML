8171f2a68b6a93c10bdda884250e03462f090e0d
Muyinda Rogers <moverr@gmail.com>
2019-11-19 14:59:47 +0300

    :Debugging, NN, THeoury as far  :

--- knn.py
+++ knn.py
@@ -1,2 +1,15 @@
-import sklearn
-from sklearn.nieghbors import train_test_split
+import numpy as np
+
+print("NUMPY {}".format(np.__version__))
+import scipy as sc
+
+print("SCIPY {}".format(sc.__version__))
+
+import pandas as pd 
+
+
+print("PANDA {}".format(pd.__version__))
+
+
+
+# from sklearn.nieghbors import train_test_split
