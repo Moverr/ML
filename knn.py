bdcc1b58d769e72296137fa428970e36843fe5ab
Muyinda Rogers <moverr@gmail.com>
2019-11-19 15:15:18 +0300

    :Debugging :

--- knn.py
+++ knn.py
@@ -7,6 +7,11 @@ print("SCIPY {}".format(sc.__version__))
 import pandas as pd 
 print("PANDA {}".format(pd.__version__))
 
+data = {
+    'Name':["John","Anna","Peter","Linda"],
+    'Location':["New Yourk","Paris","Berlin","London"],
+    'Age':[24,13,53,33]
+}
 
 
 # from sklearn.nieghbors import train_test_split
