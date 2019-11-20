dfaa728fe7b9a8c859d44e8016840cf95e81b590
Muyinda Rogers <moverr@gmail.com>
2019-11-20 11:01:54 +0300

    Debuging

--- knn.py
+++ knn.py
@@ -1,28 +1,32 @@
 # upgrading a package 
 # pip3 install --upgrade pandas --user
 import numpy as np
-print("NUMPY {}".format(np.__version__))
+# print("NUMPY {}".format(np.__version__))
 import scipy as sc
-print("SCIPY {}".format(sc.__version__))
+# print("SCIPY {}".format(sc.__version__))
 import pandas as pd 
-print("PANDA {}".format(pd.__version__))
 
-data = {
-    'Name':["John","Anna","Peter","Linda"],
-    'Location':["New Yourk","Paris","Berlin","London"],
-    'Age':[24,13,53,33]
-}
+print("Interesting Pass")
 
 
-data_pandas = pd.DataFrame(data)
+# print("PANDA {}".format(pd.__version__))
 
-print(" ****************************88 \n ")
+# data = {
+#     'Name':["John","Anna","Peter","Linda"],
+#     'Location':["New Yourk","Paris","Berlin","London"],
+#     'Age':[24,13,53,33]
+# }
 
-print(data_pandas.Name)
-print(data_pandas.Location)
-print(data_pandas.Age)
 
-print("Different  ..............................\n")
-print(data_pandas.loc[1])
+# data_pandas = pd.DataFrame(data)
+
+# print(" ****************************88 \n ")
+
+# print(data_pandas.Name)
+# print(data_pandas.Location)
+# print(data_pandas.Age)
+
+# print("Different  ..............................\n")
+# print(data_pandas.loc[1])
 
 # from sklearn.nieghbors import train_test_split
