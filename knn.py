a91979f156197877c4166536afd62ea8fac1b901
Muyinda Rogers <moverr@gmail.com>
2019-11-20 11:55:22 +0300

    Iris Loaded Data :

--- knn.py
+++ knn.py
@@ -10,7 +10,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
-irisDataset = load_iris()
+iris_dataset = load_iris()
 
 
 X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
